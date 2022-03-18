a8c0e237ccf6bc7bef8ada741549ab618a25c1be
jrhei <jannorheydon@gmail.com>
2022-03-18 18:08:14 +0800

    EXERCISE 02 DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    let completeWord='';
+    if (count < 0){
+        return 'ERROR'
+    }
+    else{
+        for (i = 1; i <= count; i++){
+            completeWord+=word;
+        }
+        return completeWord;
+    }
+    
 };
 
 // Do not edit below this line
