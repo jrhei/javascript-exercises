4b26f792fd7bd11a43a5ccb72f2424af6500d627
jrhei <jannorheydon@gmail.com>
2022-03-18 18:28:09 +0800

    EXERCISE 03 DONE

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(strings) {
+    stringsToArr = strings.split('');
+    finalIndex = stringsToArr.length - 1
+    let reversedArr=[];
+    let y = 0;
+    for(i=finalIndex; i >= 0; i--){
+        reversedArr[y] = stringsToArr[i];
+        y++;
+    }
+    let reversedWord = reversedArr.join('');
+    return reversedWord;
 };
 
 // Do not edit below this line
