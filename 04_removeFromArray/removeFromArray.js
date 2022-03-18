77624a1d0a5b41729a335f185eaa21b413794aab
jrhei <jannorheydon@gmail.com>
2022-03-18 20:17:22 +0800

    EXERCISE 04 DONE

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+    //let arrArgs = [args];
+    let newArr=[];
+    let state = false;
+    loop1:
+        for (const character of arr){
+    loop2:
+            for (let i = 0; i <= theArgs.length - 1; i++)
+            {
+                if(character === theArgs[i]){
+                    state = false;
+                    break;
+                }
+                else{
+                    state = true;
+                }
+            }
+           if (state == true){
+               newArr.push(character);
+           } 
+        }
+        return newArr;
 };
 
 // Do not edit below this line
