8a9eea7adcc8f3a13e6a4e88e26fdfd2a3ce8ce1
jrhei <jannorheydon@gmail.com>
2022-03-20 13:29:44 +0800

    EXERCISE 07 DONE

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //formula C = (F-32)* (5/9)
+  let converted = ((temp - 32) * (5/9)).toFixed(1);
+  if(converted == 0.0)return 0
+  return Number(converted);
+  
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  //formula F = ((9C+160)/5)
+  let converted = (((9*temp)+160)/5).toFixed(1);
+  if(converted == 0.0)return 0
+  return Number(converted);
 };
 
 // Do not edit below this line
