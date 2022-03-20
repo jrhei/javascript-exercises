7423920d7f3e48e61cf73298b121b7d88a90e27b
jrhei <jannorheydon@gmail.com>
2022-03-20 12:57:38 +0800

    EXERCISE 06 DONE

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //leap years divisible by 4 or 400
+    //not leap years if divisible by 100
+    remainderOne = (year % 4);
+    remainderTwo = (year % 400);
+    remainderThree = (year % 100);
 
+    if(!(remainderThree == 0) && (remainderOne == 0) || (remainderTwo == 0)  )return true;
+    return false;
 };
 
 // Do not edit below this line
