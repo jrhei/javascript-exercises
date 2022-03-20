77308250edc9a7c459d95eb246e052b0113e217f
jrhei <jannorheydon@gmail.com>
2022-03-20 13:51:40 +0800

    EXERCISE 08 DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sumArr) {
+  let sum=0;
+	for(const nums of sumArr){
+    sum+=nums;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let final=numbers[0];
+  for(i = 1; i <= numbers.length -1; i++){
+    final *= numbers[i];
+  }
+  return final;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let final = num;
+  if (num == 0)return 1
+  for(i = num - 1; i >= 1; i--){
+    final*= i;
+  }
+  return final;
 };
 
 // Do not edit below this line
