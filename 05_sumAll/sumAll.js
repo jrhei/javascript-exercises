8c25181cebc1071f7c74751b594b1f01b3f9b055
jrhei <jannorheydon@gmail.com>
2022-03-20 11:47:24 +0800

    EXERCISE 05 DONE

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let sum=0;
+    typeFirst = typeof firstNum;
+    typeSecond = typeof secondNum;
+    if (!(firstNum >= 0 && secondNum >= 0) || !(typeFirst === 'number' && typeSecond === 'number')){
+        return 'ERROR';
+    }
+    else{
+        if (firstNum < secondNum){
+            for (let i = firstNum; i <= secondNum;i++){
+                sum+=i;
+            }
+        }
+        else{
+            for (let i = secondNum; i <= firstNum;i++){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
