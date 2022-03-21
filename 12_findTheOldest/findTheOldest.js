54364bc9450cf988d6b9388b1f6af908c2c54e82
jrhei <jannorheydon@gmail.com>
2022-03-21 11:49:01 +0800

    EXERCISE 12 DONE

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personName) {
+    let age;
+    let current = 0;
+    let temp = 0;
+    let older;
+    let latestDeath;
+    const d = new Date();
+    for (const x of personName){
+        if (x.yearOfDeath === undefined){
+            age = d.getFullYear() - x.yearOfBirth;
+        }
+        else{
+            age = x.yearOfDeath - x.yearOfBirth;
+        }
+        
+        
+        if (current > age){
+            continue;
+        }
+        else if(age > current){
+            current = age;
+            older = x;
+        }
+    }
+    return older;
 };
 
 // Do not edit below this line
