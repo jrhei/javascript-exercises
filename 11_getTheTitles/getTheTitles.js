e3d65d6c6ead4b7d1b7c6156af4cebcd8c8c2e80
jrhei <jannorheydon@gmail.com>
2022-03-21 10:55:42 +0800

    EXERCISE 11 DONE

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+    let bookArr = [];
+    for (const books of bookList){
+        bookArr.push(books.title);
+    }
+    return bookArr;
 };
 
 // Do not edit below this line
