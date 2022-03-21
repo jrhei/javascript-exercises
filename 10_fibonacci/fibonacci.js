2c63cc1672dbe17a2be52f26db96ce188cc6a120
jrhei <jannorheydon@gmail.com>
2022-03-21 10:21:00 +0800

    EXERCISE 10 DONE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    //fibonacci formula
+    //F = Fn-1 + Fn
+    //1 1 2 3
+    //let converted_Number = Number(number);
+    let previous_F = 0;
+    let present_F = 1;
+    let temp;
+    if (number == 0){
+        return 0;
+    }
+
+    else if (number == 1){
+        return 1;
+    }
+    else if (number >= 2){
+        for (let i = 2; i <= number; i++){
+            temp = present_F;
+            present_F += previous_F;
+            previous_F = temp;
+        }
+        return present_F;
+    }
+    else{
+        return "OOPS";
+    }
 
 };
 
